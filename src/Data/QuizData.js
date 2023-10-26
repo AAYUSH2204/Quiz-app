8b5cdbecb0f4ecafa7e77e534bb2be72982bf407
AAYUSH2204 <aayush0026.be21@chitkara.edu.in>
2023-10-26 12:14:44 +0530

    app ready

---
+++ src/Data/QuizData.js
@@ -0,0 +1,70 @@
+export const QuizData = [
+  {
+    question: "Which language runs in a web browser?",
+    options: ["Java", "C", "Python", "JavaScript"],
+    answer: 4,
+  },
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    answer: 2,
+  },
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    answer: 1,
+  },
+  {
+    question: "What year was JavaScript launched?",
+    options: ["1996", "1995", "1994", "none of the above"],
+    answer: 2,
+  },
+  {
+    question: "What is 48,879 in hexidecimal? ",
+    options: ["0x18C1", "0xBEEF", "0xDEAD", "0X12D591"],
+    answer: 2,
+  },
+  {
+    question: "Which of the following is not a built-in module in Node.js?",
+    options: ["fs", "http", "mysql", "path"],
+    answer: 3,
+  },
+  {
+    question:
+      "What will be the result of the following JavaScript: var x = Volvo+16+4",
+    options: ["164volvo", "Volvo164", "20volvo", "Error"],
+    answer: 2,
+  },
+  {
+    question: "The 'var' and 'function' are known as ",
+    options: ["Data types", "Keywords", "Prototypes", "Declaration statements"],
+    answer: 4,
+  },
+  {
+    question:
+      "Which of these is the correct way in which we can call the javascript code?",
+    options: ["Triggering Event", "Preprocessor", "Function/Method", "RMI"],
+    answer: 3,
+  },
+  {
+    question:
+      "In JavaScript, what do we use for calling the expression for function definition?",
+    options: [
+      "Function literal",
+      "Function prototype",
+      "Function declaration",
+      "Function calling",
+    ],
+    answer: 1,
+  },
+];
